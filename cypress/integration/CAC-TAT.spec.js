e5407ca8547d51687166ece4b64336dcf04f3308
Pedro <pedroaraujo4910@gmail.com>
2023-10-31 08:28:57 -0300

    Ex extras 2 e 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,27 +10,81 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
     it('preenche os campos obrigatórios e envia o formulário', function() {
 
-        cy.get('input[id="firstName"]')
+        // Correção: dentro de get() pode ter apenas "#firstName"
+        cy.get('#firstName')
         .type('Pedro Moisés')
         .should('have.value', 'Pedro Moisés')
 
-        cy.get('input[id="lastName"]')
+        cy.get('#lastName')
         .type('Gusmão')
         .should('have.value', 'Gusmão')
 
-        cy.get('input[type="email"')
+        cy.get('#email')
         .type('rodrigo@faro.com')
         .should('have.value', 'rodrigo@faro.com')
 
-        cy.get('textarea')
-        .type('kkkk')
-        .should('have.value', 'kkkk')
+        const texto = 'Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,Batata,'
+        cy.get('#open-text-area')
+        .type(texto, {delay:0})
+        .should('have.value', texto)
 
         cy.get('button')
         .click()
 
+        //                  .sucess
         cy.get('span[class="success"]')
         .should('be.visible')
     })
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName')
+        .type('Pedro Moisés')
+        .should('have.value', 'Pedro Moisés')
 
+        cy.get('#lastName')
+        .type('Gusmão')
+        .should('have.value', 'Gusmão')
+
+        cy.get('#email')
+        .type('pedro')
+        .should('have.value', 'pedro')
+
+        cy.get('#open-text-area')
+        .type('a')
+        .should('have.value', 'a')
+
+        cy.get('button[type="submit"]')
+        .click()
+
+        cy.get('.error')
+        .should('be.visible')
+    
+    })
+
+    it.only('permanece vazio se for digitado um valor não numérico no telefone', function(){
+        cy.get('#firstName')
+        .type('Pedro Moisés')
+        .should('have.value', 'Pedro Moisés')
+
+        cy.get('#lastName')
+        .type('Gusmão')
+        .should('have.value', 'Gusmão')
+
+        cy.get('#email')
+        .type('pedro@mail.com')
+        .should('have.value', 'pedro@mail.com')
+
+        cy.get('#phone')
+        .type('e')
+        .should('not.have.value', 'e')
+
+        cy.get('#open-text-area')
+        .type('a')
+        .should('have.value', 'a')
+
+        cy.get('button[type="submit"]')
+        .click()
+
+        cy.get('.success')
+        .should('be.visible')
+    })
 })
